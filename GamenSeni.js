650b094678a85bb90fc97a90e7a3febebfe256f5
h-sasou <144661905+h-sasou@users.noreply.github.com>
2023-10-16 11:02:07 +0900

    一回目

---
+++ GamenSeni.js
@@ -0,0 +1,46 @@
+// JavaScript source code
+    // マイページボタンをクリックした時　関数
+    function RoadMypage() {
+        // マイページのURLを指定
+        var MyPageUrl = "maypage.html";
+        // 画面を遷移させる
+        window.location.href = MyPageUrl;
+    }
+
+    // 幹事ボタンをクリックした時　関数
+    function Roadkanzi() {
+        //幹事のURLを指定
+        var KanziUrl = "kanzi.html";
+        //画面を遷移させる
+        window.location.href = KanziUrl;
+    }
+
+    // お店ボタンをクリックした時　関数
+    function RoadShop() {
+        //注文選択のURLを指定
+        var OderUrl = "oder.html";
+        //画面を遷移させる
+        window.location.href = OderUrl;
+    }
+
+    // 戻るをクリックした時　関数
+    function RoadMenu() {
+        // メニューのURLを指定
+        var MenuUrl = "menu.html";
+        // 画面を遷移させる
+        window.location.href = MenuUrl;
+    }
+
+    function RoadCart() {
+        //カート確認のURLを指定
+        var CartUrl = "cart.html";
+        //画面を遷移させる
+        window.location.href = CartUrl;
+    }
+
+    function RoadOder() {
+        //注文選択のURLを指定
+        var OderUrl = "oder.html";
+        //画面を遷移させる
+        window.location.href = OderUrl;
+    }
