de21cefc18c1a151d3ef10bd78d7e3181fc0f2cf
ym4747 <144661889+ym4747@users.noreply.github.com>
2023-10-17 14:31:38 +0900

    暫定２

--- Miraben_DB/TM_Shop_Mng.sql
+++ Miraben_DB/TM_Shop_Mng.sql
@@ -8,25 +8,39 @@ Create Table
         ,ShopShi		varchar(20) not null
         ,ShopTyo		varchar(50) not null
         ,ShopBill		varchar(30)
-        ,Mon		BOOLEAN 
-        ,Tue		BOOLEAN 
-        ,Wed		BOOLEAN 
-        ,Thu		BOOLEAN
-        ,Fri		BOOLEAN 
-        ,Sat		BOOLEAN 
-        ,Sun		BOOLEAN 
-        ,Hol		BOOLEAN 
+        ,Mon		BOOLEAN DEFAULT '0'
+        ,Tue		BOOLEAN DEFAULT '0'
+        ,Wed		BOOLEAN DEFAULT '0'
+        ,Thu		BOOLEAN DEFAULT '0'
+        ,Fri		BOOLEAN DEFAULT '0'
+        ,Sat		BOOLEAN DEFAULT '0'
+        ,Sun		BOOLEAN DEFAULT '0'
+        ,Hol		BOOLEAN DEFAULT '0'
         ,TelNum		varchar(15)
         ,EigyouStart		time
         ,EigyouEnd			time
-        ,DeleteFlg		boolean not null
+        ,DeleteFlg		boolean not null DEFAULT '0'
         );
-        
         
         ALTER TABLE TM_Shop_mng
 			MODIFY ShopName VARCHAR(30) NOT NULL;
 
-        
+
+
+drop procedure ShopInsert;
+
+DELIMITER //
+
+CREATE PROCEDURE ShopInsert(in ShopName varchar(30),in ShopKen varchar(30),in ShopShi varchar(30),in ShopTyo varchar(30), ShopBill varchar(30),in Mon BOOLEAN,in Tue BOOLEAN,in Wed BOOLEAN, inThu BOOLEAN,in Fri BOOLEAN,in Sat BOOLEAN,in Sun BOOLEAN,in Hol BOOLEAN,in TelNum varchar(15),in EigyouStart time,in EigyouEnd time,in DeleteFlg boolean) 
+BEGIN
+
+	INSERT INTO TM_Shop_mng (ShopName, ShopKen, ShopShi, ShopTyo, ShopBill, Mon, Tue, Wed, Thu, Fri, Sat, Sun, Hol, TelNum, EigyouStart, EigyouEnd, DeleteFlg) 
+	VALUES (ShopName, ShopKen, ShopShi, ShopTyo, ShopBill, Mon, Tue, Wed, Thu, Fri, Sat, Sun, Hol, TelNum, EigyouStart, EigyouEnd, DeleteFlg);
+END;
+//
+
+DELIMITER ;
+
         
         
         
