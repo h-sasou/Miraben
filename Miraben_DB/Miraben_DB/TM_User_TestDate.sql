f8b05bfbc58f2200e30728d89669eb090dc15bcf
Y-nishida0922 <y-nishida@ciel-japan.jp>
2023-10-18 17:37:19 +0900

    kaetayo

---
+++ Miraben_DB/Miraben_DB/TM_User_TestDate.sql
@@ -0,0 +1,10 @@
+
+
+INSERT INTO TM_User (UserID, UserName, PassWord) VALUES ('j-murata@ciel.japan', '田村純一', 'muramura0729');
+INSERT INTO TM_User (UserID, UserName, PassWord) VALUES ('c-ciel@ciel.japan', '志位絵琉太郎', 'cerueru99');
+INSERT INTO TM_User (UserID, UserName, PassWord) VALUES ('h-sato@ciel.japan', '佐藤英人', 'ATYWddffaa');
+INSERT INTO TM_User (UserID, UserName, PassWord) VALUES ('f-fuzioka@ciel.japan', '藤岡藤太郎', 'sdfghjkl');
+INSERT INTO TM_User (UserID, UserName, PassWord) VALUES ('t-test@ciel.japan', '田中太郎', 'morimorinakaoka');
+INSERT INTO TM_User (UserID, UserName, PassWord) VALUES ('y-momizi@ciel.japan', '山村紅葉', 'momizioroi335');
+INSERT INTO TM_User (UserID, UserName, PassWord) VALUES ('mm-morioka@ciel.japan', '森岡俊太郎', 'norinori');
+INSERT INTO TM_User (UserID, UserName, PassWord) VALUES ('s-sugiyama@ciel.japan', '杉山優貴', 'yuukisugiyama');
