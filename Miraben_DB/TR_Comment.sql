6660657ad1e020e6cb2902b607da5eae9e45595a
ym4747 <144661889+ym4747@users.noreply.github.com>
2023-10-23 17:04:26 +0900

    子メンツ

--- Miraben_DB/TR_Comment.sql
+++ Miraben_DB/TR_Comment.sql
@@ -6,31 +6,28 @@ Create Table
         ,ShopID					smallint not null 
         ,FoodID					smallint not null 
 		-- ,CategoryCode			varchar(15) not null
-        ,CommentID  			smallint
+        ,CommentID  			smallint DEFAULT '0'
         ,Comment				varchar(150) not null
 		,PRIMARY KEY(UserID,ShopID,FoodID,CommentID)
         );
         
         
-        
-
-
 drop PROCEDURE InComment;
 DELIMITER //
-CREATE PROCEDURE InComment(IN input varchar(30))
+ CREATE PROCEDURE InComment(IN InputUserID varchar(30),IN InputShopID smallint,IN InputFoodID smallint,IN InputComment varchar(150))
 BEGIN
-
-
-
-DECLARE InputCoomentID Smallint;
-set InputCoomentID =  MAX(UserID, ShopID, FoodID, ComentID)+1;
-
-INSERT INTO TR_Comment (UserID, ShopID, FoodID, ComentID, Comment)
-VALUES (InputUserID, InputShopID, InputFoodID, InputCoomentIDInputComment);
-
-
-
-
+ DECLARE InputCommentID smallint;
+ set InputCommentID = COALESCE((select MAX(CommentID) + 1
+from TR_Comment
+where UserID = InputUserID
+AND ShopID = InputShopID
+AND FoodID = InputFoodID), 0);
+INSERT INTO TR_Comment (UserID, ShopID, FoodID, CommentID, Comment)
+VALUES (InputUserID, InputShopID, InputFoodID,  InputCommentID, InputComment);
 END;
 //
 DELIMITER ;
+-- コメント入力のクエリだお
+/*
+call InComment('a-ito@ciel-japan.jp','5','2','なによりもさけ！！！！');
+*/
